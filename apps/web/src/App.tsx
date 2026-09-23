c33c787abe6dbe6cd774221fd1f7a53178ead9a8
Mutiara A'yun <mutiara.ayun19@gmail.com>
2026-09-23 10:54:52 +0700

    feat(auth): redirect authenticated users away from /login

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,34 +1,40 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { AppShell } from './components/AppShell';
-import { Protected } from './components/Protected';
-import { LoginPage } from './pages/LoginPage';
-import { ExecutivePage } from './pages/ExecutivePage';
-import { FinancialPage } from './pages/FinancialPage';
-import { OperationalPage } from './pages/OperationalPage';
-import { StrategicPage } from './pages/StrategicPage';
-import { HumanCapitalPage } from './pages/HumanCapitalPage';
-import { RiskPage } from './pages/RiskPage';
-import { ProsesBisnisPage } from './pages/ProsesBisnisPage';
-import { OrganisasiPage } from './pages/OrganisasiPage';
-import { GcgEsgPage } from './pages/GcgEsgPage';
-import { PetaPage } from './pages/PetaPage';
-import { ApprovalsPage } from './pages/ApprovalsPage';
-import { InputRealisasiPage } from './pages/InputRealisasiPage';
-import { KpiMasterPage } from './pages/KpiMasterPage';
-import { SettingsPage } from './pages/SettingsPage';
-import { AdminPage } from './pages/AdminPage';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { AppShell } from "./components/AppShell";
+import { GuestOnly, Protected } from "./components/Protected";
+import { LoginPage } from "./pages/LoginPage";
+import { ExecutivePage } from "./pages/ExecutivePage";
+import { FinancialPage } from "./pages/FinancialPage";
+import { OperationalPage } from "./pages/OperationalPage";
+import { StrategicPage } from "./pages/StrategicPage";
+import { HumanCapitalPage } from "./pages/HumanCapitalPage";
+import { RiskPage } from "./pages/RiskPage";
+import { ProsesBisnisPage } from "./pages/ProsesBisnisPage";
+import { OrganisasiPage } from "./pages/OrganisasiPage";
+import { GcgEsgPage } from "./pages/GcgEsgPage";
+import { PetaPage } from "./pages/PetaPage";
+import { ApprovalsPage } from "./pages/ApprovalsPage";
+import { InputRealisasiPage } from "./pages/InputRealisasiPage";
+import { KpiMasterPage } from "./pages/KpiMasterPage";
+import { SettingsPage } from "./pages/SettingsPage";
+import { AdminPage } from "./pages/AdminPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={
+          <GuestOnly>
+            <LoginPage />
+          </GuestOnly>
+        }
+      />
       <Route
         element={
           <Protected>
             <AppShell />
           </Protected>
-        }
-      >
+        }>
         <Route index element={<ExecutivePage />} />
         <Route path="financial" element={<FinancialPage />} />
         <Route path="operational" element={<OperationalPage />} />
@@ -42,7 +48,10 @@ export default function App() {
         <Route path="approvals" element={<ApprovalsPage />} />
         <Route path="input-realisasi" element={<InputRealisasiPage />} />
         {/* Input Kontrak Manajemen kini digabung ke "Manajemen KPI" (tab Dokumen KM) */}
-        <Route path="input-kontrak" element={<Navigate to="/kpi-master" replace />} />
+        <Route
+          path="input-kontrak"
+          element={<Navigate to="/kpi-master" replace />}
+        />
         <Route path="kpi-master" element={<KpiMasterPage />} />
         <Route path="settings" element={<SettingsPage />} />
         <Route path="admin" element={<AdminPage />} />
